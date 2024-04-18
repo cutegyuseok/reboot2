bb11ac1f4967a55fef79b72179c02c86e3a21bbe
cutegyuseok <yigyuseok@gmail.com>
2024-04-18 21:24:20 +0900

    init

---
+++ src/main/java/org/example/reboot2/Reboot2Application.java
@@ -0,0 +1,13 @@
+package org.example.reboot2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Reboot2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Reboot2Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/reboot2/Reboot2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.reboot2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Reboot2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
